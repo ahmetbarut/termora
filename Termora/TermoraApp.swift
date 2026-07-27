e491357dc6e31894902816d35516bade07f10665
Ahmet Barut <ahmetbarut@Ahmet-MacBook-Air.local>
2026-07-27 15:08:49 +0300

    Initial Commit

---
+++ Termora/TermoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  TermoraApp.swift
+//  Termora
+//
+//  Created by Ahmet Barut on 27.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct TermoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
